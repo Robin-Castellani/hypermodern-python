5140c4e85ef89a650ddbb5ede3d0410c7586f281
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-20 11:33:52 +0200

    Test: add noxfile.py for Nox

---
+++ noxfile.py
@@ -0,0 +1,14 @@
+"""
+Nox configuration file.
+"""
+
+import nox
+
+
+@nox.session(python=['3.8', '3.7'])
+def test(session):
+    # install dependencies
+    # poetry is not part of the environment
+    session.run('poetry', 'install', external=True)
+    # run the tests
+    session.run('pytest', '--cov')
