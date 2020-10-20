ceb70439414d0976e29930fe0482a4c2da83e237
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-20 10:08:58 +0200

    Feat: give meaningful error codes & get language

--- src/my_hypermodern_python/console.py
+++ src/my_hypermodern_python/console.py
@@ -10,21 +10,29 @@ import requests
 from . import __version__
 
 
-API_URL = 'https://en.wikipedia.org/api/rest_v1/page/random/summary'
-
-
 @click.command()
 @click.version_option(version=__version__)
-def main():
+@click.option('--lang', default='en', help='Which language do you want?')
+def main(lang):
     """
     Get a random fact from the Wikipedia API
-
-    :return: None
     """
+    api_url = 'https://en.wikipedia.org/api/rest_v1/page/random/summary'
 
-    with requests.get(API_URL) as response:
-        response.raise_for_status()  # if bad status, raises exception
-        data = response.json()  # the API provide its data in json
+    api_url = api_url.replace('en', lang, 1)
+
+    try:
+        with requests.get(api_url) as response:
+            response.raise_for_status()  # if bad status, raises exception
+            data = response.json()  # the API provide its data in json
+    except requests.HTTPError:
+        raise click.ClickException(
+            f'Wikipedia API not reachable OR given language "{lang}" not valid'
+        )
+    except requests.exceptions.ConnectionError:
+        raise click.ClickException(
+            f'Given language "{lang}" not valid'
+        )
 
     title = data['title']
     extract = data['extract']
