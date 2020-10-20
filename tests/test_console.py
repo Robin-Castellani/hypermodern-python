39530e540ee177180ddd9e3cbf4ffa75eaf522cf
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-20 15:54:48 +0200

    Test: add more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,6 +4,7 @@ Test the ``console.py``.
 
 import click.testing
 import pytest
+import requests
 
 from my_hypermodern_python import console
 
@@ -44,3 +45,32 @@ def test_main_prints_extract(runner, mock_requests_get):
     output_extract = result.output.split('\n')[1:-1]
     string_extract = ' '.join(output_extract)
     assert string_extract == 'Lorem ipsum dolor sit amet'
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert 'en.wikipedia.org' in args[0]
+
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception('Boom')
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+
+def test_main_fails_on_http_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.HTTPError
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+
+def test_main_fails_on_connection_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.exceptions.ConnectionError
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
