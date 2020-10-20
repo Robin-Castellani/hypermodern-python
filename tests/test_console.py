1f2186c68bcb7c9cee0d55936665a6c0406a9a6c
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-20 10:34:11 +0200

    Test: add first test

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+"""
+Test the ``console.py``.
+"""
+
+import click.testing
+
+from my_hypermodern_python import console
+
+
+def test_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
