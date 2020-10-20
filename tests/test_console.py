6f2c0e93bce0d34fdb5895daa3b7e626ece98d69
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-20 16:18:24 +0200

    Test: handle RequestException

--- tests/test_console.py
+++ tests/test_console.py
@@ -74,3 +74,9 @@ def test_main_fails_on_connection_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.exceptions.ConnectionError
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert 'Error' in result.output
