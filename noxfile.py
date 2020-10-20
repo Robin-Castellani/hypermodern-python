c37bb0e98396bee57358f7c43ec454eaae77a172
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-20 11:35:12 +0200

    Test: Nox can take positional arguments

--- noxfile.py
+++ noxfile.py
@@ -7,8 +7,11 @@ import nox
 
 @nox.session(python=['3.8', '3.7'])
 def test(session):
+    # get the positional arguments form the CLI
+    # default to --cov
+    args = session.posargs or ['--cov']
     # install dependencies
     # poetry is not part of the environment
     session.run('poetry', 'install', external=True)
     # run the tests
-    session.run('pytest', '--cov')
+    session.run('pytest', *args)
