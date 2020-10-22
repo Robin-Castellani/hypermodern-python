aa614de35e44c798f7b41aeea751f2bc85eb734e
robin-castellani <robin.castellani@alumni.unitn.it>
2020-10-22 11:50:14 +0200

    Chore: add type annotation in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,15 @@
 Common pytest fixtures.
 """
 
+from unittest.mock import Mock
+
+from _pytest.config import Config
 import pytest
+from pytest_mock import MockFixture
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
+def mock_requests_get(mocker: MockFixture) -> Mock:
     mock = mocker.patch("requests.get")
     # the request is used as a context manager and its json attribute
     #  is accessed to get the data from the API
@@ -17,5 +21,5 @@ def mock_requests_get(mocker):
     return mock
 
 
-def pytest_configure(config):
+def pytest_configure(config: Config) -> None:
     config.addinivalue_line("markers", "e2e: mark as ent-to-end test.")
